5c343343d5276253aec4c9b8f7f195923d26456b
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2025-12-16 22:13:40 +0100

    sc

--- app/dashboard/tickets/new/page.tsx
+++ app/dashboard/tickets/new/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/auth'
 import Sidebar from '@/components/dashboard/Sidebar'
 import CreateTicketForm from '@/components/tickets/CreateTicketForm'
 
